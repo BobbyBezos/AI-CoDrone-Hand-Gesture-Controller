8dc1410f2c8e20b581bbe6d9f41d025bcf9aba60
BobbyBezos <maxwalsh1906@gmail.com>
2026-06-08 14:14:00 +1000

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report
+
+def print_confusion_matrix(y_true, y_pred, report=True):
+    labels = sorted(list(set(y_true)))
+    cmx_data = confusion_matrix(y_true, y_pred, labels=labels)
+    
+    df_cmx = pd.DataFrame(cmx_data, index=labels, columns=labels)
+ 
+    fig, ax = plt.subplots(figsize=(7, 6))
+    sns.heatmap(df_cmx, annot=True, fmt='g' ,square=False)
+    ax.set_ylim(len(set(y_true)), 0)
+    plt.show()
+    
+    if report:
+        print('Classification Report')
+        print(classification_report(y_test, y_pred))
+
+Y_pred = model.predict(X_test)
+y_pred = np.argmax(Y_pred, axis=1)
+
+print_confusion_matrix(y_test, y_pred)
